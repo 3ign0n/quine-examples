de8461123b4c5476029bcfbd6df83dbc1de4a559
TAKEDA hiroyuki(aka 3ign0n) <take6a.hir0yuki@gmail.com>
2011-01-11 23:26:21 +0900

    add some files

---
+++ rabbit_quine.rb
@@ -0,0 +1,25 @@
+eval$s=%w'b="BAhsK1T////////////////////nf/z///8Hf
+uD///8f8ID/////wAP+////Dw/8/////3jw/////8fA/////wc
+A/v///wcA4P///wcAAP7//w  8AAPD/AT4A   AMD/8fFhAIb/
+8Y8PAAD/5x//gAf/jz8ed      uP8fzw      e8HDA/wf++Y
+AA/P//4w8B8P//8YAHn//       /jw/       wA//////hH/
+7///////8DAA==";n=Mars      hal.       load(b.unpa
+ck("m")[0]);e="eval$s=%w    "<<3      9<<($s*3);o=
+"";j=-1;0.upto(50*25-1){|i   |o<<     ((n[i]==1)?e
+[j+=1]:32);o<<((i%50==49)?1   0:      "")};o[-7,6]
+=""<<39<<".join";puts(o)#              b="BAhsK1T/
+///////////////////nf/z                  ///8HfuD/
+//8f8ID/////wAP+////D                      w/8////
+/3jw/////8fA/////wcA                        /v///w
+cA4P///        wcAAP                        7//w8A
+APD/A   T4AAM   D/8fF    hA          Ib    /8Y8PAA
+D/5   x//gAf/j   z8edu                    P8fzwe8H
+DA/  wf++YAA/   P//4w8B8       P//8     YAHn///jw/
+wA   //////h   H/7/    // /// /8   DAA  ==";n=Mars
+hal   .loa   d(b.       unpa    ck(       "m")[0])
+;e="e      val$s=%w  "<<39       <          <($s*3
+);o="";j=-1;0.upto   (50*25-    1           ){|i|o
+<<((n[i]==1)?e[   j+=1       ]:32     );o<<  ((i%5
+0==49)?10:"")};o   [-7,6        ]=""<<      39<<".
+join";puts(o)#b="BAhsK1T///    ////////    ///////
+//nf/z///8HfuD///8f8ID/////wAP+////Dw/8/////'.join
